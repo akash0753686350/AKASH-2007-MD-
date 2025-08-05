30958a395055ee715b7aa2f9dc237cb519407695
AKASH-2007-MD <abiakash171@gmail.com>
2025-08-05 22:19:29 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -44,8 +44,8 @@ async (conn, mek, m, { from }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: '*𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃*',
+                    newsletterJid: '120363418238379872@newsletter',
+                    newsletterName: '*AKASH-2007-MD💫*',
                     serverMessageId: 143
                 }            
             }
