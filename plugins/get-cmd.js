8699fe92ea16ecf0165150b051a92d15f74938d8
AKASH-2007-MD <abiakash171@gmail.com>
2025-08-08 13:32:32 +0530

    Update get-cmd.js

--- plugins/get-cmd.js
+++ plugins/get-cmd.js
@@ -50,8 +50,8 @@ ${truncatedCode}
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: 'ArslanMD Official',
+                    newsletterJid: '120363418238379872@newsletter',
+                    newsletterName: 'AkashMD Official',
                     serverMessageId: 143
                 }
             }
