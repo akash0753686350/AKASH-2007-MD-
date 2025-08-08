fa7589ab7a4a59184fff53aaa66d18974577e904
AKASH-2007-MD <abiakash171@gmail.com>
2025-08-08 12:40:39 +0530

    Update check-uptime.js

--- plugins/check-uptime.js
+++ plugins/check-uptime.js
@@ -119,8 +119,8 @@ ${config.DESCRIPTION}`;
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: config.OWNER_NAME || 'Arslan-MD Official',
+                    newsletterJid: '120363418238379872@newsletter',
+                    newsletterName: config.OWNER_NAME || 'AKASH-MD-V4 Official',
                     serverMessageId: 143
                 }
             }
