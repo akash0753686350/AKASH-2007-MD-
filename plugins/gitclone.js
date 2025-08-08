c73db2510d1632dd4330fded5d456f1b76406853
AKASH-2007-MD <abiakash171@gmail.com>
2025-08-08 13:39:16 +0530

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -55,8 +55,8 @@ cmd({
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363348739987203@newsletter',
-          newsletterName: 'ArslanMD Official',
+          newsletterJid: '120363418238379872@newsletter',
+          newsletterName: 'AkashMD Official',
           serverMessageId: 143
         }
       }
